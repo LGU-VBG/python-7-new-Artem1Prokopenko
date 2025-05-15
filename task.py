5ca2e6f03e5367468215499d41b23d79363858f8
ArtemProkop <stnprokop2@gmail.com>
2025-05-15 22:00:27 +0300

    да

--- task.py
+++ task.py
@@ -1,1 +1,23 @@
+#1
+def merge_lists_to_dict(k, v):
+    return dict(zip(k, v))
 
+word = ["f", "a", "b", "c"]
+price = [50, 30, 60, 40]
+
+result1 = merge_lists_to_dict(k=word, v=price)
+result2 = merge_lists_to_dict(word, v=[100, 200, 300])
+
+print(result1)
+print(result2)
+
+
+#2
+def update_car_info(**cu):
+    car = cu
+    car['is_available'] = True
+    return car
+
+result = update_car_info(brand='Nissan', price=7000000)
+
+print(result)
